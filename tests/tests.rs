1f652a70303eefda2a5029c05f7e8f2709423b5b
May Tusek <tusek.may@gmail.com>
2023-08-24 10:59:18 -0700

    Reduces requirements.

--- tests/tests.rs
+++ tests/tests.rs
@@ -7,14 +7,14 @@ use this_returns::generate_model;
 #[test]
 pub fn test_generate_model() {
   #[generate_model]
-  #[derive(Debug, Dummy, Serialize, Deserialize)]
+  #[derive(Dummy, Serialize)]
   struct TestStruct {
     a: String,
     b: u32,
   }
 
   #[generate_model]
-  #[derive(Debug, Dummy, Serialize, Deserialize)]
+  #[derive(Dummy, Serialize)]
   struct TestStruct2 {
     a: TestStruct,
     b: u32,
